df7a056d461d9d83852ebe3da76e7640707a3108
Jaime Reyes <126992552+jaimereyess@users.noreply.github.com>
2023-11-23 13:56:52 +0000

    new style

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -13,14 +13,12 @@ function Block({ element, symbol, mass, atomic, type }: BlockProps) {
     const elementColor = getColorByType(type)
 
     return (
-        <>
-            <div className={`block ${elementColor} font-bold`}>
-                <p className="atomic">{atomic}</p>
-                <p className="mass">{mass}</p>
-                {symbol}
-                <span className="span-block">{element}</span>
-            </div>
-        </>
+        <div className={`block ${elementColor} font-bold`}>
+            <p className="atomic">{atomic}</p>
+            <p className="mass">{mass}</p>
+            {symbol}
+            <span className="span-block">{element}</span>
+        </div>
 
     )
 }
